6719224300f0a700a78c99911b98a3fbcb5d56ed
k3vin7 <kevinc02@naver.com>
2026-05-25 22:43:57 +0900

    Add poem page (윤동주 자화상) with view and URL

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def poem(request):
+    return render(request, 'blog/poem.html', {})
